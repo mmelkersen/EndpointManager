8d00647fce3b16519c21ec51c56a7b53a6070010
MMelkersen <matttiasmelkersen@outlook.com>
2021-02-07 22:35:23 +0100

    Ready for github

--- BlobStorage/Upload_IPULogs_to_blob.ps1
+++ BlobStorage/Upload_IPULogs_to_blob.ps1
@@ -1,10 +1,10 @@
 #=====================================================================================================
-# Created on:   07.02.2021
+# Created on:   29.01.2021
 # Created by:   Mattias Melkersen
 # Version:	    0.1 
 # Mail:         mm@mindcore.dk
 # Twitter:      MMelkersen
-# Function:     Sample script to upload Inplace upgrade logs to Azure Blob storage
+# Function:     Sample script to upload files to Azure Blob storage
 # 
 # Special Thanks: 
 # Tom Degreef https://www.oscc.be/sccm/Logging-in-the-cloud-Part-1/
@@ -34,11 +34,11 @@ $TSHostname = $env:computername
 
 $hostname = ($TSHostname).tolower()
 $Timestamp = get-date -f yyyy-MM-dd-HH-mm-ss
-$localpath = "C:\temp\Logs\$hostname-$Timestamp"
+$localpath = "C:\temp\Logs\$hostname-IPU-$Timestamp"
 
 New-Item -ItemType Directory -Path $localpath -Force
 New-Item -ItemType Directory -Path $localpath\IPU -Force
-Start-Process -FilePath $Setupdiag -ArgumentList "/Output:C:\temp\logs\IPU\Results.log"
+Start-Process -FilePath $Setupdiag -ArgumentList "/Output:$($localpath)\IPU\Results.log" -Wait
 
 Compress-Archive -Path $localpath -DestinationPath "C:\temp\Logs\$hostname-IPU-$Timestamp.zip"
 
