b53fd30ccb27151fdb5507341a36cb4e66ca2705
MMelkersen <matttiasmelkersen@outlook.com>
2021-04-22 20:44:46 +0200

    Ready for Github

--- Microsoft .NET Framework/Remediate_.Net3.5_Feature.ps1
+++ Microsoft .NET Framework/Remediate_.Net3.5_Feature.ps1
@@ -15,12 +15,26 @@
 
 #>
 
+[string]$Logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Baseline.log" 
+Function Write_Log
+	{
+	param(
+	$Message_Type, 
+	$Message
+	)
+		$MyDate = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)  
+		Add-Content $LogFile  "$MyDate - $Message_Type : $Message"  
+	} 
+
 Try 
     {
+        Write_Log -Message_Type "INFO" -Message "Enabling NetFx3..."
         Enable-WindowsOptionalFeature -Online -FeatureName 'NetFx3' -NoRestart
         Write-Host "Installed .Net 3.5 Successfully"
+        Write_Log -Message_Type "INFO" -Message "Enabled NetFx3 Successfully"
     }
     catch
     {
         Write-Host ".net 3.5 installation failed"
+        Write_Log -Message_Type "WARNING" -Message "Failed to enable NetFx3"
     }
