32954bb0a2b8988547ed6aed14e35d54ee60e3d4
MMelkersen <matttiasmelkersen@outlook.com>
2021-04-27 19:17:54 +0200

    test1

--- Microsoft .NET Framework/Remediate_.Net3.5_Feature.ps1
+++ Microsoft .NET Framework/Remediate_.Net3.5_Feature.ps1
@@ -3,18 +3,25 @@
  
  Created on:    22.04.2021
  Created by:    Mattias Melkersen
- Version:       0.1  
+ Version:       0.2  
  Mail:          mm@mindcore.dk
  twitter:       @mmelkersen
  Function:      Remediation script if .net 3.5 windows feature has been enabled. Use it together with ProActive Remediation.
 
- 
+ History log
+ v0.1 - Working natively with downloading content only from Microsoft
+ v0.2 - Thanks to @maleroytw for elaborating on devices with state: DisabledWithPayloadRemoved. This version will be able to get payload from Github and add it using /Limitaccess
+
  This script is provided As Is
  Compatible with Windows 10 and later
 ======================================================================================================
 
 #>
 
+[String]$GithubPath = "https://github.com/mmelkersen/EndpointManager/blob/main/Microsoft%20.NET%20Framework/1909/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~en-US~.cab"
+[String]$Repository = "EndpointManager"
+[string]$Owner = "MattiasMelkersen"
+[String]$path = "Microsoft .NET Framework"
 [string]$Logfile = "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\Baseline.log" 
 Function Write_Log
 	{
@@ -25,6 +32,22 @@ Function Write_Log
 		$MyDate = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)  
 		Add-Content $LogFile  "$MyDate - $Message_Type : $Message"  
 	} 
+
+    #Get build number
+    $WindowsVersion = Get-ComputerInfo WindowsVersion
+    Write_Log -Message_Type "INFO" -Message $WindowsVersion
+
+    #Check if C:\Windows\temp\WinSXS exists
+    if (!(test-path "C:\Windows\temp"))
+        {
+            New-Item -ItemType Directory -Path "C:\temp"
+        }
+
+    #Get files from Github
+    Invoke-WebRequest -Uri $GithubPath - -OutFile "C:\temp\test.cab"
+    
+
+    dism /online /enable-feature /featurename:NetFX3 /all /Source:d:sourcessxs /LimitAccess
 
 Try 
     {
